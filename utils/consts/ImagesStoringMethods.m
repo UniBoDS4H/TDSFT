0c935b18fd016ff76a4d4a341c93c6bae74c8149
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-03-16 11:32:18 +0100

    feat(images): enum for images storing methods.

---
+++ utils/consts/ImagesStoringMethods.m
@@ -0,0 +1,12 @@
+classdef ImagesStoringMethod
+    % Enumeration class for images' storing methods
+    
+    properties (Constant)
+        INT_8 = 'uint8'
+        INT_16 = 'uint16'
+        INT_32 = 'uint32'
+        INT_64 = 'uint64'
+    end
+
+end
+
