6944701717974a9c2cf6254f53c78731e83b315f
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-03-17 15:49:27 +0100

    fix(images): fix imTo8bit function.

--- utils/imTo8bit.m
+++ utils/imTo8bit.m
@@ -1,4 +1,4 @@
-function imTo8bit(img)
+function cImg = imTo8bit(img)
 % AUTHOR: Lorenzo Drudi (E-mail: lorenzo.drudi5@studio.unibo.it)
 % DATE: March 15, 2022
 % NAME: TDSFT (version 1.0)
@@ -11,7 +11,7 @@ function imTo8bit(img)
 
 % TODO: check im2uint8 (the docs says it works only with uint16).
 if isa(img, ImagesStoringMethods.INT_16)
-    if max(newImg(:)) > 2^12
+    if max(img(:)) > 2^12
         cImg  = 255.*double(img)./(2^16-1); 
     else
         cImg  = 255.*double(img)./(2^12-1); 
@@ -25,7 +25,7 @@ else
 end
 
 % Check if the storing method of the converted image is `uint8`
-if ~isa(cImg, ImagesStoringMethods.UINT_8)
+if ~isa(cImg, ImagesStoringMethods.INT_8)
     error('Error. \nThe image is not uint8');
 end
 
