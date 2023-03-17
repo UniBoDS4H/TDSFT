d7c4a013ca9ca0193abf3699ea15a49fb3bef60b
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-03-17 09:26:54 +0100

    feat: add startup script.

---
+++ runStartup.m
@@ -0,0 +1,11 @@
+function runStartup() 
+% AUTHOR: Lorenzo Drudi (E-mail: lorenzo.drudi5@studio.unibo.it)
+% DATE: March 17, 2022
+% NAME: TDSFT (version 1.0)
+%
+% DESCRIPTION: This function is used to add the folders of the project to the search path
+
+addpath(genpath('utils'));
+addpath(genpath('api'));
+
+end
