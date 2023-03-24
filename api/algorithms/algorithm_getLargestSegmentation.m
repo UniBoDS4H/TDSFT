ddf2e61cfea89d9724d4e5bf38fc535d279497f6
Lorenzo Drudi <lorenzodrudi11@gmail.com>
2023-03-24 16:12:16 +0100

    feat(images): implement largest segmentation algorithm.

--- api/algorithms/algorithm_getLargestSegmentation.m
+++ api/algorithms/algorithm_getLargestSegmentation.m
@@ -1,4 +1,4 @@
-function res = getLargestSegmentation(segmentations) 
+function res = algorithm_getLargestSegmentation(segmentations) 
 % AUTHOR: Lorenzo Drudi (E-mail: lorenzo.drudi5@studio.unibo.it)
 % DATE: March 24, 2022
 % NAME: TDSFT (version 1.0)
@@ -13,14 +13,21 @@ function res = getLargestSegmentation(segmentations)
 %       Fuse all the segmentations together overlapping them and getting the largest possible segmentation which contains all the inputs.
 %
 
+    % initialize the overlap
+    first = segmentations{1};
+    [m,n] = size(first);
+    overlap = zeros(m, n, 'uint8');
+
     % overlap all the segmentations
-    overlap = [];
-    for seg = segmentations:
+    for i=1:length(segmentations)
+        seg = uint8(~segmentations{i});
         overlap = overlap + seg;
     end 
 
+    overlap = imbinarize(overlap);
+    overlap = uint8(~overlap);
+
     % fill the resulting segmentation and get the perimeter
-    fillOverlap = imfill(overlap, 'holes');
+    % fillOverlap = imfill(overlap, 'holes');
     res = bwperim(fillOverlap);
-    
 end
